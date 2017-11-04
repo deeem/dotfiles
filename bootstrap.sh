c9f02b86fa69f48ba12dd9d6ea983108ec852d52
deeem <karpov.dimon@gmail.com>
2017-11-04 20:19:57 +0200

    Update bootstrap.sh
    sudo not needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install playbook dependesis
-sudo ansible-galaxy install -r ansible/requirements.yml
+ansible-galaxy install -r ansible/requirements.yml
 
 #Run ansible
 ansible-playbook --ask-become-pass ansible/playbook.yml -i ansible/hosts --extra-vars "dotfilespath=`pwd`"
